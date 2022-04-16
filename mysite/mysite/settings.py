c487d1a381aed9426353ec152922d24717a46e3c
Jonna Hyypiä <jonna.hyypia@helsinki.fi>
2022-04-16 12:33:19 +0300

    Delete password validators

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -89,23 +89,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
 PASSWORD_RESET_TIMEOUT = float('inf')
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
