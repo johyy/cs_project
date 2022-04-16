be774bf948a51ca4749960103884827c07ecb0f4
Jonna Hyypiä <jonna.hyypia@helsinki.fi>
2022-04-16 10:59:03 +0300

    Change secret key to None

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j+a$5gkzwu=w@h=r#)aab3)b)sml3xgf19nf19y!+v7ptox0u7'
+SECRET_KEY = 'None'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
